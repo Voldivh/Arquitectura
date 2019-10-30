a8ef9f926f1d439a41ac9f8b776acb2530d11139
Voldivh <51831786+Voldivh@users.noreply.github.com>
2019-10-30 09:48:34 -0500

    Update CSAI.v

--- CSAI.v
+++ CSAI.v
@@ -1,7 +1,7 @@
 //=======================================================
 //  MODULE Definition
 //=======================================================
-module CSAI #(parameter DATAWIDTH_BUS=32)
+module CSAI #(parameter DATAWIDTH_BUS=11)
 (
 	//////////// INPUTS //////////
 	CLK, 
@@ -18,30 +18,39 @@ module CSAI #(parameter DATAWIDTH_BUS=32)
 //=======================================================
 //  PORT declarations
 //=======================================================
-	input					 CLK;
-	input					 RESET;
-	input					 ACK;
-	input			[10:0] CS;
-	output reg	[10:0] OUT;
+	input		CLK;
+	input		RESET;
+	input		ACK;
+	input		[10:0] CS;
+	output		[10:0] OUT;
 //=======================================================
 //  REG/WIRE declarations
 //=======================================================
+reg [DATAWIDTH_BUS-1:0] Counter_Register;
+reg [DATAWIDTH_BUS-1:0] Counter_Signal;
 //=======================================================
 //  Structural coding
 //=======================================================
-// INPUT LOGIC: SEQUENTIAL
-	always@(posedge CLK)
-		begin 
-			if(RESET == 1)
-				OUT <= 0;
-			else if(ACK==0)
-				OUT = CS + 1;
-			else
-				OUT = CS;
-		end
 // INPUT LOGIC: COMBINATIONAL
+always @(CS)
+begin
+	if(ACK==0)
+		Counter_Signal = CS + 1;
+	else
+		Counter_Signal = CS;
+end	
+// INPUT LOGIC: SEQUENTIAL
+always@(posedge CLK, posedge RESET)
+begin 
+	if(RESET == 1)
+		Counter_Register <= 0;
+	else
+		Counter_Register <= Counter_Signal;
+end
+
 //=======================================================
 //  Outputs
 //=======================================================
 // OUTPUT LOGIC : COMBINATIONAL
+assign OUT = Counter_Register;
 endmodule
